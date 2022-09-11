41fe7f65c40a8a798a57124f31654d4dfcf8031c
Saurabh Patel <60851848+100rab-h@users.noreply.github.com>
2022-09-11 10:32:06 +0530

    Create children_sum_property.cpp
    codeStudio
    https://www.codingninjas.com/codestudio/problems/childrensumproperty_790723?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0

---
+++ children_sum_property.cpp
@@ -0,0 +1,41 @@
+/*************************************************************
+
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode < T > *left;
+        BinaryTreeNode < T > *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+void changeTree(BinaryTreeNode < int > * root) {
+    // Write your code here.
+    if (!root) return;
+    int child = 0;
+    if (root->left) child += root->left->data;
+    if (root->right) child += root->right->data;
+    
+    if (child < root->data) {
+        if (root->left) root->left->data = root->data;
+        if (root->right) root->right->data = root->data;
+    } else {
+        root->data = child;
+    }
+    
+    changeTree(root->left);
+    changeTree(root->right);
+    
+    int tot = 0;
+    if (root->left) tot += root->left->data;
+    if (root->right) tot += root->right->data;
+    if (root->left or root->right) root->data = tot;
+}  
